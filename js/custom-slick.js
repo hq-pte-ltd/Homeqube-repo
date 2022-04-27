ee8fa25e94525b7093e7156c5eb03dd86d39e969
Ched28 <75325713+Ched28@users.noreply.github.com>
2022-04-27 14:21:27 +0800

    Chedrick Update

---
+++ js/custom-slick.js
@@ -0,0 +1,54 @@
+$(function(){
+    
+    $('.slider').slick({
+      
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: false,
+        ease: true,
+        prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-arrow-left"></i></span>',
+        nextArrow: '<span class="next_arrow"><i class="fa-solid fa-arrow-right"></i></span>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              prevArrow: false,
+              nextArrow: false
+              
+            }
+          },
+          {
+            breakpoint: 375,
+            settings: {
+              centerMode: true,
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              unslick: true,
+              dots: false,
+              prevArrow: false,
+              nextArrow: false
+            },
+           
+          }
+          
+         
+        ]
+      });
+
+    
+      
+});
